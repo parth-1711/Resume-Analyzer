ae33bf2739b3da049288a05f5b9d036db3ce1ae0
parth-1711 <parthirache8@gmail.com>
2025-03-01 15:00:31 +0530

    added route for / and removed unnecessary files

--- Backend/resume-analyzer/src/main/java/com/resumeanalyzer/resume_analyzer/controller/ResumeController.java
+++ Backend/resume-analyzer/src/main/java/com/resumeanalyzer/resume_analyzer/controller/ResumeController.java
@@ -7,10 +7,7 @@ import com.resumeanalyzer.resume_analyzer.model.Resume;
 import com.resumeanalyzer.resume_analyzer.service.ResumeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -21,6 +18,11 @@ public class ResumeController {
     @Autowired
     public ResumeController(ResumeService resumeService){
         this.resumeService=resumeService;
+    }
+
+    @GetMapping("/")
+    public String hello(){
+        return "hello";
     }
 
     @PostMapping(value="/parse", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
