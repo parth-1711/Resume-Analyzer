c77cd0140794b0a678e024cc03d6d17ba5769a9e
parth-1711 <parthirache8@gmail.com>
2025-03-02 20:05:49 +0530

    Updated vite config file

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,7 +4,10 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  allowedHosts: ["ec2-13-60-92-220.eu-north-1.compute.amazonaws.com"],
+  server: {
+    allowedHosts: ["ec2-13-60-92-220.eu-north-1.compute.amazonaws.com"],
+    host: "0.0.0.0", // Allows external access
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
